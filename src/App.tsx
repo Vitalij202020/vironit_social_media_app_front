9c4b5f6be999459fc77592961f196519602d7ea9
Vitalij202020 <vaskouski.v.v@gmail.com>
2022-06-08 21:50:49 +0300

    style(src): add some basic style and layout, realized it with material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+      <>
+          <Header/>
+          <HomePage/>
+          {/*<RegisterPage/>*/}
+          {/*<LoginPage/>*/}
+      </>
   );
 }
 
